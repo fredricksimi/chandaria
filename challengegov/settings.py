ded40bc10e74932b884cc60ea8a502318feefe2b
Fredrick Simi <fkemosi@gmail.com>
2020-06-02 23:48:23 +0300

    we can do this all night

--- challengegov/settings.py
+++ challengegov/settings.py
@@ -140,7 +140,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # )
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # CKEditor SETTINGS
 CKEDITOR_UPLOAD_PATH = 'uploads/'
 CKEDITOR_CONFIGS = {
