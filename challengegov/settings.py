b3d22bd9b49c0eff73eab819ddb6fd246ab2e97e
Fredrick Simi <fkemosi@gmail.com>
2020-06-02 23:29:51 +0300

    changeds

--- challengegov/settings.py
+++ challengegov/settings.py
@@ -1,5 +1,5 @@
 import os
-
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -154,7 +154,8 @@ CKEDITOR_CONFIGS = {
     },
 }
 CKEDITOR_IMAGE_BACKEND = 'pillow'
-
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
 # ACCOUNT_EMAIL_REQUIRED = True
 # ACCOUNT_USERNAME_REQUIRED = False
 # ACCOUNT_SIGNUP_PASSWORD_ENTER_TWICE = False
